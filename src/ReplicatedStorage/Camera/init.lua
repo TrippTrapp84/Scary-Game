8a97970b90d30bbe8988da0e2d1540fc912cc0e7
i_Zolox <33046239+izolox@users.noreply.github.com>
2021-03-28 15:01:23 -0500

    New camera system

--- src/ReplicatedStorage/Camera/init.lua
+++ src/ReplicatedStorage/Camera/init.lua
@@ -12,7 +12,36 @@ local NULL = {}
 local Mouse = Players.LocalPlayer:GetMouse()
 local UserInputServ = game:GetService("UserInputService")
 local RotSpeed = 1
+local MaxLinearAngle = 90
 
+--// LOCAL FUNCTIONS
+local function ErrorFunction(x)
+    -- constants
+    local a1 =  0.254829592
+    local a2 = -0.284496736
+    local a3 =  1.421413741
+    local a4 = -1.453152027
+    local a5 =  1.061405429
+    local p  =  0.3275911
+
+    -- Save the sign of x
+    local sign = 1
+    if x < 0 then
+        sign = -1
+    end
+    x = math.abs(x)
+
+    -- A&S formula 7.1.26
+    local t = 1.0/(1.0 + p*x)
+    local y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*math.exp(-x*x)
+
+    return sign*y
+end
+
+local function SmoothLerp(x)
+    local z = math.sqrt(32) * (x - 0.5)
+    return 0.5 * ErrorFunction(z) * 1/0.99993662792787
+end
 
 --// CONSTRUCTOR
 local Handler = {}
@@ -72,12 +101,23 @@ function Handler.new(Data)
         if Obj.Character then
             CameraRot = CFrame.Angles(0, math.rad(Yaw), 0) * CFrame.Angles(math.rad(Pitch), 0, 0)
             CameraRot = CFrame.fromMatrix(
-                HRP.CFrame:PointToWorldSpace(Vector3.new(0, 1, 0)) + CameraRot.YVector,
+                HRP.CFrame:PointToWorldSpace(Vector3.new(0, 1.25, -.25)) + Vector3.new(CameraRot.YVector.X,0, CameraRot.YVector.Z) * .5,
                 CameraRot.XVector,
                 CameraRot.YVector,
                 CameraRot.ZVector
             )
-            Obj:SetCFrame(CameraRot)
+            Obj:SetCFrame(Obj.Camera.CFrame:Lerp(CameraRot, 0.8))
+            CameraRot = Obj.Camera.CFrame
+            local HRPZ = -Vector3.new(CameraRot.ZVector.X,0, CameraRot.ZVector.Z).Unit
+            local Tolorance = (1 - math.abs(CameraRot.ZVector.Y)) * MaxLinearAngle
+            local targetHRPCFrame = CFrame.lookAt(HRP.Position, HRP.Position + HRPZ)
+            local AngleDiff = targetHRPCFrame:ToObjectSpace(HRP.CFrame)
+            local DotResult = AngleDiff.ZVector.Unit:Dot(Vector3.new(0, 0, 1))
+            local Angle = math.acos(DotResult) * 180/math.pi * math.sign(AngleDiff.ZVector.X)
+            Angle = math.clamp(Angle, -Tolorance, Tolorance)
+            AngleDiff = CFrame.Angles(0, math.rad(Angle), 0)
+            --print(Angle, Tolorance)
+            HRP.CFrame = targetHRPCFrame * AngleDiff
             for _, bp in pairs(Obj.Character:GetChildren()) do
                 if bp:IsA("MeshPart") then
                     bp.LocalTransparencyModifier = 0
@@ -89,7 +129,7 @@ function Handler.new(Data)
     Obj.Connections[3] = UserInputServ.InputChanged:Connect(function(input)
         if input.UserInputType == Enum.UserInputType.MouseMovement then
             local DeltaX, DeltaY = -input.Delta.X, -input.Delta.Y
-            Pitch = math.clamp(Pitch + DeltaY/RotSpeed, -90, 90)
+            Pitch = math.clamp(Pitch + DeltaY/RotSpeed, -85, 85)
             Yaw += DeltaX/RotSpeed
         end
     end)
