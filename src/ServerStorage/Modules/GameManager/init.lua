e9f2afdb521b320ae9dec0349fcef30d2ff55d3f
TrippTrapp84 <HoldenTyler8654@gmail.com>
2021-03-28 07:10:06 -0500

    Initial commit on GameMaster module

--- src/ServerStorage/Modules/GameManager/init.lua
+++ src/ServerStorage/Modules/GameManager/init.lua
@@ -8,6 +8,63 @@ local StateMachine = require(RepStore.Network.Utility.States)
 local NULL = {}
 
 --// VARIABLES
+local States = {
+    Pre_Game = {
+        StartGame = "Match_Day"
+    },
+    Match_Day = {
+        Transition = "Match_Day_To_Night",
+        EndGame = "Post_Game"
+    },
+    Match_Day_To_Night = {
+        Transition = "Match_Night",
+        EndGame = "Post_Game"
+    },
+    Match_Night = {
+        Transition = "Match_Night_To_Day",
+        EndGame = "Post_Game"
+    },
+    Match_Night_To_Day = {
+        Transition = "Match_Day",
+        EndGame = "Post_Game"
+    },
+    Post_Game = {
+        StartPreGame = "Pre_Game"
+    }
+}
+
+local StateSwitchFunctions = {
+    Pre_Game___Match_Day = function(self)
+        
+    end,
+    Match_Day___Match_Day_To_Night = function(self)
+        
+    end,
+    Match_Day_To_Night___Match_Night = function(self)
+        
+    end,
+    Match_Night___Match_Night_To_Day = function(self)
+        
+    end,
+    Match_Night_To_Day___Match_Day = function(self)
+        
+    end,
+    Match_Day___Post_Game = function(self)
+        
+    end,
+    Match_Day_To_Night___Post_Game = function(self)
+        
+    end,
+    Match_Night___Post_Game = function(self)
+        
+    end,
+    Match_Night_To_Day___Post_Game = function(self)
+        
+    end,
+    Post_Game___Pre_Game = function(self)
+        
+    end
+}
 
 --// CONSTRUCTOR
 local Handler = {}
@@ -27,10 +84,22 @@ function Handler.new(Data)
 
     for i,v in pairs(DefaultValues()) do
         Obj[i] = Data[i] == nil and v or Data[i]
-        if Obj[i] == NULL then error("Missing data for CLASS_NAME_HERE constructor:",i) end
+        if Obj[i] == NULL then error("Missing data for GameManager constructor:",i) end
     end
 
     --// INITIALIZATION
+    Obj.GameStateMachine = StateMachine.new("Pre_Game",States)
+
+    Obj.Connections = {}
+
+    Obj.Connections[1] = Obj.GameStateMachine.StateChanged:Connect(function(OldState,NewState)
+        local SwitchFunction = StateSwitchFunctions[OldState .. "___" .. NewState]
+        if not SwitchFunction then
+            error("CRITICAL ERROR: UNHANDLED STATE TRANSITION EXCEPTION")
+        else
+            SwitchFunction(Obj)
+        end
+    end)
 
     return Obj
 end
