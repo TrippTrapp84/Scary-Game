eb3f8b76716e46a6a1675530446a0090cea8f5c0
TrippTrapp84 <HoldenTyler8654@gmail.com>
2021-03-28 12:03:38 -0500

    Fixed state functions for gamemanager looking terible in the table

---
+++ src/ServerStorage/Modules/GameManager/StateChangedFunctions.lua
@@ -0,0 +1,34 @@
+local StateSwitchFunctions = {
+    Pre_Game___Match_Day = function(self)
+        
+    end,
+    Match_Day___Match_Day_To_Night = function(self)
+        
+    end,
+    Match_Day_To_Night___Match_Night = function(self)
+        
+    end,
+    Match_Night___Match_Night_To_Day = function(self)
+        
+    end,
+    Match_Night_To_Day___Match_Day = function(self)
+        
+    end,
+    Match_Day___Post_Game = function(self)
+        
+    end,
+    Match_Day_To_Night___Post_Game = function(self)
+        
+    end,
+    Match_Night___Post_Game = function(self)
+        
+    end,
+    Match_Night_To_Day___Post_Game = function(self)
+        
+    end,
+    Post_Game___Pre_Game = function(self)
+        
+    end
+}
+
+return StateSwitchFunctions
