e5ca9c24be594acde5c9d229f6e45be8fa3311c8
lwrcman <sm3136@engr.ship.edu>
2021-04-02 11:20:55 -0400

    Removed unneeded comments

--- src/ReplicatedStorage/Initialize/Server.lua
+++ src/ReplicatedStorage/Initialize/Server.lua
@@ -18,8 +18,8 @@ local function Init()
 
     Services.ApiManager = ApiManager.new({
         url = ROOT_API,
-        new_connections_api = "/v1/server/identify",  -- TEMP NAME
-        websocket_api = "/v1/websocket", -- Maybe? TEMP NAME
+        new_connections_api = "/v1/server/identify",
+        websocket_api = "/v1/websocket",
         terminating_api = "/v1/server/terminating"
     })
 
